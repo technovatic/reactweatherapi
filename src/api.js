bcdb7a4116c19071972c3ef5ce854494417b8ec0
Vishal Kelur <technovatic@gmail.com>
2024-04-08 13:30:29 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "618f1afdd2msha6eb92edce0c6eap16da52jsn42e530e6d0da",// enter your rapid api key here
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "56769b2c00931bdfa86ca2e0a74f9c19"; // enter your key from openweather API
